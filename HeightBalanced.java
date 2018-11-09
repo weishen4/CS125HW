71a1efd083b40f922658161a5f3b7600a7cf9bd8
weishen4 <42952470+weishen4@users.noreply.github.com>
2018-11-09 15:02:05 -0600

    Create HeightBalanced.java
    height balanced first edtion

---
+++ HeightBalanced.java
@@ -0,0 +1,32 @@
+public class YourBinaryTree extends BinaryTree {
+    protected boolean heightBalanced() {
+        return heightBalanced(root);
+    }
+
+    private boolean heightBalanced(Node cur) {
+        if (cur == null) {
+            return true;
+        }
+        if (Math.abs(countLevel(cur.left) - countLevel(cur.right)) > 1) {
+            return false;
+        }
+        return heightBalanced(cur.left) && heightBalanced(cur.right);
+    }
+
+    private int countLevel(Node cur) {
+        //this is added after second run, remember this code
+        if (cur == null) {
+            return 0;
+        }
+
+        int leftLevel = 0;
+        int rightLevel = 0;
+        if (cur.left != null) {
+            leftLevel += 1 + countLevel(cur.left);
+        }
+        if (cur.right != null) {
+            rightLevel += 1 + countLevel(cur.right);
+        }
+        return Math.max(leftLevel, rightLevel);
+    }
+}
